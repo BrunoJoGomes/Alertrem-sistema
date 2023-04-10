becbbd08948b74e74677cbb071c585011836adc5
Nathan Santos <79227411+NathanSnt@users.noreply.github.com>
2023-04-10 17:06:56 -0300

    frmCadastrarEstacao terminada

---
+++ alertrem/sistemaAlertrem/frmCadastrarEstacao.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace sistemaAlertrem
+{
+    partial class frmCadastrarEstacao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gpbEstacao = new System.Windows.Forms.GroupBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtLocalizacao = new System.Windows.Forms.TextBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblLocalizacao = new System.Windows.Forms.Label();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.gpbEstacao.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gpbEstacao
+            // 
+            this.gpbEstacao.Controls.Add(this.txtNome);
+            this.gpbEstacao.Controls.Add(this.txtLocalizacao);
+            this.gpbEstacao.Controls.Add(this.txtCodigo);
+            this.gpbEstacao.Controls.Add(this.lblNome);
+            this.gpbEstacao.Controls.Add(this.lblLocalizacao);
+            this.gpbEstacao.Controls.Add(this.lblCodigo);
+            this.gpbEstacao.Controls.Add(this.btnCadastrar);
+            this.gpbEstacao.Controls.Add(this.btnLimpar);
+            this.gpbEstacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gpbEstacao.Location = new System.Drawing.Point(12, 12);
+            this.gpbEstacao.Name = "gpbEstacao";
+            this.gpbEstacao.Size = new System.Drawing.Size(723, 247);
+            this.gpbEstacao.TabIndex = 0;
+            this.gpbEstacao.TabStop = false;
+            this.gpbEstacao.Text = "Dados sobre a estação";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(317, 47);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(395, 29);
+            this.txtNome.TabIndex = 1;
+            // 
+            // txtLocalizacao
+            // 
+            this.txtLocalizacao.Location = new System.Drawing.Point(138, 102);
+            this.txtLocalizacao.Multiline = true;
+            this.txtLocalizacao.Name = "txtLocalizacao";
+            this.txtLocalizacao.Size = new System.Drawing.Size(574, 62);
+            this.txtLocalizacao.TabIndex = 2;
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(95, 47);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(101, 29);
+            this.txtCodigo.TabIndex = 9;
+            this.txtCodigo.TabStop = false;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(239, 50);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(72, 24);
+            this.lblNome.TabIndex = 4;
+            this.lblNome.Text = "Nome:";
+            // 
+            // lblLocalizacao
+            // 
+            this.lblLocalizacao.AutoSize = true;
+            this.lblLocalizacao.Location = new System.Drawing.Point(6, 117);
+            this.lblLocalizacao.Name = "lblLocalizacao";
+            this.lblLocalizacao.Size = new System.Drawing.Size(126, 24);
+            this.lblLocalizacao.TabIndex = 3;
+            this.lblLocalizacao.Text = "Localização:";
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(6, 47);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(83, 24);
+            this.lblCodigo.TabIndex = 2;
+            this.lblCodigo.Text = "Código:";
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(570, 187);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(142, 40);
+            this.btnCadastrar.TabIndex = 3;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(396, 187);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(142, 40);
+            this.btnLimpar.TabIndex = 4;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVoltar.Location = new System.Drawing.Point(593, 279);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(142, 40);
+            this.btnVoltar.TabIndex = 5;
+            this.btnVoltar.Text = "Voltar";
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            this.btnVoltar.Click += new System.EventHandler(this.btnVoltar_Click);
+            // 
+            // frmCadastrarEstacao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(747, 341);
+            this.Controls.Add(this.btnVoltar);
+            this.Controls.Add(this.gpbEstacao);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmCadastrarEstacao";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmCadastrarEstacao";
+            this.gpbEstacao.ResumeLayout(false);
+            this.gpbEstacao.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gpbEstacao;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtLocalizacao;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblLocalizacao;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnVoltar;
+    }
+}
