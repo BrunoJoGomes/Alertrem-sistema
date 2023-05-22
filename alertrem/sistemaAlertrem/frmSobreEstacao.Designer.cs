181fba68b82a143b236fec110843ce27a94a7bc0
Nathan Santos <79227411+NathanSnt@users.noreply.github.com>
2023-05-22 13:41:16 -0300

    acrescentado tela sobre estação

---
+++ alertrem/sistemaAlertrem/frmSobreEstacao.Designer.cs
@@ -0,0 +1,226 @@
+﻿
+namespace sistemaAlertrem
+{
+    partial class frmSobreEstacao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbcSobreEstacao = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblEndereco1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lblEndereco2 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.lblEndereco3 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.tbcSobreEstacao.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbcSobreEstacao
+            // 
+            this.tbcSobreEstacao.Controls.Add(this.tabPage1);
+            this.tbcSobreEstacao.Controls.Add(this.tabPage2);
+            this.tbcSobreEstacao.Controls.Add(this.tabPage3);
+            this.tbcSobreEstacao.Controls.Add(this.tabPage4);
+            this.tbcSobreEstacao.Location = new System.Drawing.Point(12, 12);
+            this.tbcSobreEstacao.Name = "tbcSobreEstacao";
+            this.tbcSobreEstacao.SelectedIndex = 0;
+            this.tbcSobreEstacao.Size = new System.Drawing.Size(776, 364);
+            this.tbcSobreEstacao.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.lblEndereco3);
+            this.tabPage1.Controls.Add(this.textBox5);
+            this.tabPage1.Controls.Add(this.lblEndereco2);
+            this.tabPage1.Controls.Add(this.textBox4);
+            this.tabPage1.Controls.Add(this.lblEndereco1);
+            this.tabPage1.Controls.Add(this.textBox3);
+            this.tabPage1.Controls.Add(this.lblNome);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.lblCodigo);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(768, 338);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Sobre a estação";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(768, 338);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Adicionar características";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(768, 338);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Cadastrar característica";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(768, 338);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Cadastrar estado operacional";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(117, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(127, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(73, 38);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(43, 13);
+            this.lblCodigo.TabIndex = 1;
+            this.lblCodigo.Text = "Código:";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(73, 73);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(38, 13);
+            this.lblNome.TabIndex = 3;
+            this.lblNome.Text = "Nome:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(117, 66);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(127, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // lblEndereco1
+            // 
+            this.lblEndereco1.AutoSize = true;
+            this.lblEndereco1.Location = new System.Drawing.Point(73, 111);
+            this.lblEndereco1.Name = "lblEndereco1";
+            this.lblEndereco1.Size = new System.Drawing.Size(65, 13);
+            this.lblEndereco1.TabIndex = 5;
+            this.lblEndereco1.Text = "Endereço 1:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(144, 104);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // lblEndereco2
+            // 
+            this.lblEndereco2.AutoSize = true;
+            this.lblEndereco2.Location = new System.Drawing.Point(73, 147);
+            this.lblEndereco2.Name = "lblEndereco2";
+            this.lblEndereco2.Size = new System.Drawing.Size(65, 13);
+            this.lblEndereco2.TabIndex = 7;
+            this.lblEndereco2.Text = "Endereço 2:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(144, 140);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 6;
+            // 
+            // lblEndereco3
+            // 
+            this.lblEndereco3.AutoSize = true;
+            this.lblEndereco3.Location = new System.Drawing.Point(73, 184);
+            this.lblEndereco3.Name = "lblEndereco3";
+            this.lblEndereco3.Size = new System.Drawing.Size(65, 13);
+            this.lblEndereco3.TabIndex = 9;
+            this.lblEndereco3.Text = "Endereço 3:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(144, 177);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 8;
+            // 
+            // frmSobreEstacao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbcSobreEstacao);
+            this.Name = "frmSobreEstacao";
+            this.Text = "frmSobreEstacao";
+            this.tbcSobreEstacao.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tbcSobreEstacao;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Label lblEndereco3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label lblEndereco2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label lblEndereco1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
