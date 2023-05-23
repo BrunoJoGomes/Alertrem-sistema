4a6bdd3a8e2ce58d30e01b1648e43ee1c776642b
Nathan Santos <79227411+NathanSnt@users.noreply.github.com>
2023-05-23 16:25:48 -0300

    nada por enquanto.

--- alertrem/sistemaAlertrem/frmFuncionarios.cs
+++ alertrem/sistemaAlertrem/frmFuncionarios.cs
@@ -37,7 +37,7 @@ namespace sistemaAlertrem
 
         public void carregaDados()
         {
-            string commandString = "select codigo as 'Código', nome as 'Nome', usuario as 'Usuário', data_cadastro as 'Data de criação' from tb_funcionarios";
+            string commandString = "select codigo as 'Código', nome as 'Nome', data_cadastro as 'Data de criação' from tb_funcionarios";
 
             MySqlCommand comm = new MySqlCommand
             {
