a026ae20d10407fe28a74978aa3132c31d64b62b
NathanSnt <natan-silva-2011@hotmail.com>
2023-05-26 22:19:22 -0300

    Atualização.

--- alertrem/sistemaAlertrem/Program.cs
+++ alertrem/sistemaAlertrem/Program.cs
@@ -16,7 +16,7 @@ namespace sistemaAlertrem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmEstac()); 
+            Application.Run(new frmLogin() ); 
         }
     }
 }
