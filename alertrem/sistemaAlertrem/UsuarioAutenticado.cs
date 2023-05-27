4ae36e349aa53567f09dbdb32c55fc3a73c79ca1
NathanSnt <natan-silva-2011@hotmail.com>
2023-05-26 22:17:01 -0300

    Agora o usuário autenticado é conhecido.

---
+++ alertrem/sistemaAlertrem/UsuarioAutenticado.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sistemaAlertrem
+{
+    public static class UsuarioAutenticado
+    {
+        public static int CodigoUsuarioAutenticado { get; set; }
+        public static string NomeUsuarioAutenticado { get; set; }
+    }
+}
