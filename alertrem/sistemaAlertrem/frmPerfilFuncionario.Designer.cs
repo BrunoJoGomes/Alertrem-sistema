8aacd646e35aace4a4aa0ef6b3d7031761cbd084
NathanSnt <natan-silva-2011@hotmail.com>
2023-05-27 18:26:52 -0300

    Corrigido tabIndex.

--- alertrem/sistemaAlertrem/frmPerfilFuncionario.Designer.cs
+++ alertrem/sistemaAlertrem/frmPerfilFuncionario.Designer.cs
@@ -49,45 +49,50 @@ namespace sistemaAlertrem
             // lblFuncionario
             // 
             this.lblFuncionario.AutoSize = true;
-            this.lblFuncionario.Location = new System.Drawing.Point(32, 46);
+            this.lblFuncionario.Location = new System.Drawing.Point(43, 57);
+            this.lblFuncionario.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblFuncionario.Name = "lblFuncionario";
-            this.lblFuncionario.Size = new System.Drawing.Size(121, 13);
+            this.lblFuncionario.Size = new System.Drawing.Size(160, 17);
             this.lblFuncionario.TabIndex = 0;
             this.lblFuncionario.Text = "Selecione o funcionário:";
             // 
             // cbbFuncionario
             // 
             this.cbbFuncionario.FormattingEnabled = true;
-            this.cbbFuncionario.Location = new System.Drawing.Point(153, 38);
+            this.cbbFuncionario.Location = new System.Drawing.Point(204, 47);
+            this.cbbFuncionario.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cbbFuncionario.Name = "cbbFuncionario";
-            this.cbbFuncionario.Size = new System.Drawing.Size(213, 21);
-            this.cbbFuncionario.TabIndex = 1;
+            this.cbbFuncionario.Size = new System.Drawing.Size(283, 24);
+            this.cbbFuncionario.TabIndex = 0;
             this.cbbFuncionario.SelectedIndexChanged += new System.EventHandler(this.cbbFuncionario_SelectedIndexChanged);
             // 
             // lblNomeDeUsu
             // 
             this.lblNomeDeUsu.AutoSize = true;
-            this.lblNomeDeUsu.Location = new System.Drawing.Point(32, 145);
+            this.lblNomeDeUsu.Location = new System.Drawing.Point(43, 178);
+            this.lblNomeDeUsu.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNomeDeUsu.Name = "lblNomeDeUsu";
-            this.lblNomeDeUsu.Size = new System.Drawing.Size(90, 13);
+            this.lblNomeDeUsu.Size = new System.Drawing.Size(120, 17);
             this.lblNomeDeUsu.TabIndex = 2;
             this.lblNomeDeUsu.Text = "Nome de usuário:";
             // 
             // txtNomeUsuario
             // 
             this.txtNomeUsuario.Enabled = false;
-            this.txtNomeUsuario.Location = new System.Drawing.Point(153, 137);
+            this.txtNomeUsuario.Location = new System.Drawing.Point(204, 169);
+            this.txtNomeUsuario.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtNomeUsuario.MaxLength = 50;
             this.txtNomeUsuario.Name = "txtNomeUsuario";
-            this.txtNomeUsuario.Size = new System.Drawing.Size(213, 20);
+            this.txtNomeUsuario.Size = new System.Drawing.Size(283, 22);
             this.txtNomeUsuario.TabIndex = 3;
             // 
             // lblSenha
             // 
             this.lblSenha.AutoSize = true;
-            this.lblSenha.Location = new System.Drawing.Point(32, 207);
+            this.lblSenha.Location = new System.Drawing.Point(43, 255);
+            this.lblSenha.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblSenha.Name = "lblSenha";
-            this.lblSenha.Size = new System.Drawing.Size(93, 13);
+            this.lblSenha.Size = new System.Drawing.Size(122, 17);
             this.lblSenha.TabIndex = 4;
             this.lblSenha.Text = "Senha de acesso:";
             // 
@@ -95,19 +100,21 @@ namespace sistemaAlertrem
             // 
             this.txtSenha.Enabled = false;
             this.txtSenha.Font = new System.Drawing.Font("Wingdings", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.txtSenha.Location = new System.Drawing.Point(153, 200);
+            this.txtSenha.Location = new System.Drawing.Point(204, 246);
+            this.txtSenha.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtSenha.MaxLength = 50;
             this.txtSenha.Name = "txtSenha";
             this.txtSenha.PasswordChar = 'l';
-            this.txtSenha.Size = new System.Drawing.Size(213, 20);
+            this.txtSenha.Size = new System.Drawing.Size(283, 23);
             this.txtSenha.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(32, 259);
+            this.label1.Location = new System.Drawing.Point(43, 319);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.Size = new System.Drawing.Size(123, 17);
             this.label1.TabIndex = 5;
             this.label1.Text = "Confirme a senha:";
             // 
@@ -115,20 +122,22 @@ namespace sistemaAlertrem
             // 
             this.txtConfirmaSenha.Enabled = false;
             this.txtConfirmaSenha.Font = new System.Drawing.Font("Wingdings", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.txtConfirmaSenha.Location = new System.Drawing.Point(153, 259);
+            this.txtConfirmaSenha.Location = new System.Drawing.Point(204, 319);
+            this.txtConfirmaSenha.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtConfirmaSenha.MaxLength = 50;
             this.txtConfirmaSenha.Name = "txtConfirmaSenha";
             this.txtConfirmaSenha.PasswordChar = 'l';
-            this.txtConfirmaSenha.Size = new System.Drawing.Size(213, 20);
+            this.txtConfirmaSenha.Size = new System.Drawing.Size(283, 23);
             this.txtConfirmaSenha.TabIndex = 5;
             // 
             // btnVoltar
             // 
             this.btnVoltar.Image = ((System.Drawing.Image)(resources.GetObject("btnVoltar.Image")));
-            this.btnVoltar.Location = new System.Drawing.Point(32, 317);
+            this.btnVoltar.Location = new System.Drawing.Point(43, 390);
+            this.btnVoltar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnVoltar.Name = "btnVoltar";
-            this.btnVoltar.Size = new System.Drawing.Size(101, 95);
-            this.btnVoltar.TabIndex = 6;
+            this.btnVoltar.Size = new System.Drawing.Size(135, 117);
+            this.btnVoltar.TabIndex = 8;
             this.btnVoltar.Text = "&Voltar";
             this.btnVoltar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.btnVoltar.UseVisualStyleBackColor = true;
@@ -137,10 +146,11 @@ namespace sistemaAlertrem
             // btnCriarEditar
             // 
             this.btnCriarEditar.Image = ((System.Drawing.Image)(resources.GetObject("btnCriarEditar.Image")));
-            this.btnCriarEditar.Location = new System.Drawing.Point(442, 26);
+            this.btnCriarEditar.Location = new System.Drawing.Point(589, 32);
+            this.btnCriarEditar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCriarEditar.Name = "btnCriarEditar";
-            this.btnCriarEditar.Size = new System.Drawing.Size(101, 95);
-            this.btnCriarEditar.TabIndex = 7;
+            this.btnCriarEditar.Size = new System.Drawing.Size(135, 117);
+            this.btnCriarEditar.TabIndex = 1;
             this.btnCriarEditar.Text = "&Criar/Editar usuário";
             this.btnCriarEditar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.btnCriarEditar.UseVisualStyleBackColor = true;
@@ -149,10 +159,11 @@ namespace sistemaAlertrem
             // btnSalvar
             // 
             this.btnSalvar.Image = ((System.Drawing.Image)(resources.GetObject("btnSalvar.Image")));
-            this.btnSalvar.Location = new System.Drawing.Point(442, 137);
+            this.btnSalvar.Location = new System.Drawing.Point(589, 169);
+            this.btnSalvar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSalvar.Name = "btnSalvar";
-            this.btnSalvar.Size = new System.Drawing.Size(101, 95);
-            this.btnSalvar.TabIndex = 8;
+            this.btnSalvar.Size = new System.Drawing.Size(135, 117);
+            this.btnSalvar.TabIndex = 6;
             this.btnSalvar.Text = "&Salvar";
             this.btnSalvar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.btnSalvar.UseVisualStyleBackColor = true;
@@ -161,10 +172,11 @@ namespace sistemaAlertrem
             // btnLimpar
             // 
             this.btnLimpar.Image = ((System.Drawing.Image)(resources.GetObject("btnLimpar.Image")));
-            this.btnLimpar.Location = new System.Drawing.Point(442, 247);
+            this.btnLimpar.Location = new System.Drawing.Point(589, 304);
+            this.btnLimpar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnLimpar.Name = "btnLimpar";
-            this.btnLimpar.Size = new System.Drawing.Size(101, 95);
-            this.btnLimpar.TabIndex = 9;
+            this.btnLimpar.Size = new System.Drawing.Size(135, 117);
+            this.btnLimpar.TabIndex = 7;
             this.btnLimpar.Text = "&Limpar";
             this.btnLimpar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.btnLimpar.UseVisualStyleBackColor = true;
@@ -173,26 +185,28 @@ namespace sistemaAlertrem
             // txtCodFunc
             // 
             this.txtCodFunc.Enabled = false;
-            this.txtCodFunc.Location = new System.Drawing.Point(153, 89);
+            this.txtCodFunc.Location = new System.Drawing.Point(204, 110);
+            this.txtCodFunc.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCodFunc.MaxLength = 50;
             this.txtCodFunc.Name = "txtCodFunc";
-            this.txtCodFunc.Size = new System.Drawing.Size(213, 20);
+            this.txtCodFunc.Size = new System.Drawing.Size(283, 22);
             this.txtCodFunc.TabIndex = 2;
             // 
             // lblCodigoFunc
             // 
             this.lblCodigoFunc.AutoSize = true;
-            this.lblCodigoFunc.Location = new System.Drawing.Point(32, 97);
+            this.lblCodigoFunc.Location = new System.Drawing.Point(43, 119);
+            this.lblCodigoFunc.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCodigoFunc.Name = "lblCodigoFunc";
-            this.lblCodigoFunc.Size = new System.Drawing.Size(113, 13);
+            this.lblCodigoFunc.Size = new System.Drawing.Size(150, 17);
             this.lblCodigoFunc.TabIndex = 11;
             this.lblCodigoFunc.Text = "Código do funcionário:";
             // 
             // frmPerfilFuncionario
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 450);
+            this.ClientSize = new System.Drawing.Size(779, 554);
             this.Controls.Add(this.txtCodFunc);
             this.Controls.Add(this.lblCodigoFunc);
             this.Controls.Add(this.btnLimpar);
@@ -209,6 +223,7 @@ namespace sistemaAlertrem
             this.Controls.Add(this.lblFuncionario);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "frmPerfilFuncionario";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
